221a2a02a464baed8172f6cb9080f7e667e94ff3
big.rice.eater <34292181+BigRiceEater@users.noreply.github.com>
2019-04-12 22:51:03 +0800

    Project scaffold

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx?)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Gary Hung',
+      template: 'src/index.html'
+    })
+  ],
+  devServer: {
+    publicPath: '/',
+    contentBase: path.join(__dirname, '/dist/'),
+    port: 3000
+  }
+};
