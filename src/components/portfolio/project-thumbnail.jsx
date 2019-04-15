737113a5a70d1f497627b25bb4cc93bf9591ce3d
big.rice.eater <34292181+BigRiceEater@users.noreply.github.com>
2019-04-15 19:12:48 +0800

    Add back controls

--- src/components/portfolio/project-thumbnail.jsx
+++ src/components/portfolio/project-thumbnail.jsx
@@ -4,12 +4,7 @@ import Carousel from 'nuka-carousel';
 const ProjectThumbnail = (props) => {
   const { thumbnails = [] } = props;
   return (
-    <Carousel
-      autoplay
-      dragging
-      autoplayReverse
-      renderCenterLeftControls={null}
-      renderCenterRightControls={null}>
+    <Carousel autoplay dragging autoplayReverse framePadding='0px 10px'>
       {thumbnails.map((uri) => (
         <img src={uri} style={{ objectFit: 'cover', marginTop: '5px' }} />
       ))}
