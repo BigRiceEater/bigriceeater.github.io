0931644de0237a9ff0aa5faf1a6540db769db386
big.rice.eater <34292181+BigRiceEater@users.noreply.github.com>
2019-04-15 18:39:04 +0800

    Remove left/right controls on carousel

--- src/components/portfolio/project-thumbnail.jsx
+++ src/components/portfolio/project-thumbnail.jsx
@@ -4,7 +4,12 @@ import Carousel from 'nuka-carousel';
 const ProjectThumbnail = (props) => {
   const { thumbnails = [] } = props;
   return (
-    <Carousel autoplay dragging autoplayReverse framePadding='0px 50px'>
+    <Carousel
+      autoplay
+      dragging
+      autoplayReverse
+      renderCenterLeftControls={null}
+      renderCenterRightControls={null}>
       {thumbnails.map((uri) => (
         <img src={uri} style={{ objectFit: 'cover', marginTop: '5px' }} />
       ))}
