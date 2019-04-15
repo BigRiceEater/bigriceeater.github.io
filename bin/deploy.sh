1a3c017cedd39b877aa195a36010c53da3f332c9
big.rice.eater <34292181+BigRiceEater@users.noreply.github.com>
2019-04-15 15:05:49 +0800

    Add current date to deploy commit

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+DATE=`date '+%Y-%m-%d %H:%M:%S'`
+
 # A user github page builds directly from master
 git checkout master
 git merge dev
 npm run build
 git add ./dist
-git commit 
+git commit -m "$DATE"
 git subtree push --prefix dist origin master
 git checkout dev
