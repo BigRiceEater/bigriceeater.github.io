27eed19b0a1db21d6714b45d88ec990b4468c643
big.rice.eater <34292181+BigRiceEater@users.noreply.github.com>
2019-04-17 12:08:47 +0800

    Add view my portfolio to github hero

--- src/components/home/github-source.jsx
+++ src/components/home/github-source.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Icons from './../../modules/icon/icon-mapper';
 
 class GithubSource extends Component {
@@ -8,16 +9,23 @@ class GithubSource extends Component {
       <section className='hero is-medium has-background-light is-bold'>
         <div className='hero-body'>
           <div className='container has-text-centered'>
-            <span className='fa-8x'>{Icons.brand.githubSquare}</span>
-            <h1 className='title'>
+            <span className='fa-8x' style={{ lineHeight: '100%' }}>
+              {Icons.brand.githubSquare}
+            </span>
+            {/* <h1 className='title'>
               Github{' '}
               <a href='https://github.com/BigRiceEater' target='_blank'>
                 <span className='fa-sm'>{Icons.utility.externalLink}</span>
               </a>
-            </h1>
+            </h1> */}
             <h2 className='subtitle'>
               All my code is open source with live demo on github pages
             </h2>
+            <Link
+              className='button is-rounded is-primary is-large'
+              to='/portfolio'>
+              View My Portfolio
+            </Link>
           </div>
         </div>
       </section>
