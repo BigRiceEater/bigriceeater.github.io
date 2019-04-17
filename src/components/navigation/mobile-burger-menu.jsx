53b66d77cc904819d053a2a99fa64eb6153a1dad
big.rice.eater <34292181+BigRiceEater@users.noreply.github.com>
2019-04-17 10:45:51 +0800

    Add link for Home

--- src/components/navigation/mobile-burger-menu.jsx
+++ src/components/navigation/mobile-burger-menu.jsx
@@ -1,26 +1,46 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Icons from './../../modules/icon/icon-mapper';
+import Portfolio from '../../views/portfolio';
 
 class MobileBurgerMenu extends Component {
+  state = {
+    links: [
+      {
+        to: '/',
+        icon: Icons.utility.unknown,
+        label: 'Home'
+      },
+      {
+        to: '/portfolio',
+        icon: Icons.utility.code,
+        label: 'Portfolio'
+      }
+    ]
+  };
+
   getMenuClasses = () => {
     let classes = 'navbar-menu';
     if (this.props.isMenuActive) classes += ' is-active';
     return classes;
   };
 
+  navLink = ({ to, icon, label, onNavigation }) => {
+    return (
+      <Link key={to} to={to} className='navbar-item' onClick={onNavigation}>
+        {icon} &nbsp; {label}
+      </Link>
+    );
+  };
+
   render() {
+    const { links } = this.state;
     return (
       <div id='burger-menu' className={this.getMenuClasses()}>
         <div className='navbar-start'>
-          <Link
-            to='/portfolio'
-            className='navbar-item'
-            onClick={() => {
-              this.props.onNavigation();
-            }}>
-            {Icons.utility.code} &nbsp; Portfolio
-          </Link>
+          {links.map((link, index) =>
+            this.navLink({ ...link, onNavigation: this.props.onNavigation })
+          )}
         </div>
       </div>
     );
