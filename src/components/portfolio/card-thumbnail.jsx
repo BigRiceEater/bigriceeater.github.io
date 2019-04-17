7d490b38f9be693c1ea12847c1f98545dcc8e66a
big.rice.eater <34292181+BigRiceEater@users.noreply.github.com>
2019-04-17 16:43:13 +0800

    Move card thumbnail style to variable

--- src/components/portfolio/card-thumbnail.jsx
+++ src/components/portfolio/card-thumbnail.jsx
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
+
 class CardThumbnail extends Component {
-  state = {};
   render() {
     const { thumbnail } = this.props.data;
     return (
       <div className='card-image'>
         <figure className='image'>
-          <img
-            src={thumbnail}
-            alt='card image'
-            style={{
-              objectFit: 'cover',
-              marginTop: '5px',
-              minHeight: '180px'
-            }}
-          />
+          <img src={thumbnail} alt='card image' style={this.thumbnailStyle} />
         </figure>
       </div>
     );
   }
+
+  thumbnailStyle = {
+    objectFit: 'cover',
+    marginTop: '5px',
+    minHeight: '180px'
+  };
 }
 
 export default CardThumbnail;
