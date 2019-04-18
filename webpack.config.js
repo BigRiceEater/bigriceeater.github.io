9faf05beb463884a0aba22d5221ea25b09772474
big.rice.eater <34292181+BigRiceEater@users.noreply.github.com>
2019-04-18 11:44:52 +0800

    Extract css to separate file in build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   entry: './src/index.js',
@@ -22,8 +24,18 @@ module.exports = {
         use: 'babel-loader'
       },
       {
-        test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              fallback: 'style-loader',
+              hmr: process.env.NODE_ENV === 'development'
+            }
+          },
+          'css-loader',
+          'sass-loader'
+        ]
       },
       {
         test: /\.(png|svg|jpg|gif|ico)$/,
@@ -36,6 +48,10 @@ module.exports = {
       title: 'Big Rice',
       template: 'src/index.html',
       favicon: 'assets/favicon.ico'
+    }),
+    new MiniCssExtractPlugin({
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css'
     })
   ],
   devServer: {
